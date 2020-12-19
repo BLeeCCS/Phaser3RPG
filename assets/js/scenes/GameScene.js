4bfc652a160d45d1174d66dae52af0a1c6d0596a
bleeccs@gmail.com <ledbrandon@yahoo.com>
2020-12-19 12:47:05 -0800

    Transition Scene tutorial

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -3,11 +3,50 @@ class GameScene extends Phaser.Scene {
         super('Game');
     }
 
-    preload() {
-
-    }
-
     create() {
-        
+        // audio
+        var goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 1 });
+
+        var button = this.add.image(100, 100, 'button1');
+        button.setOrigin(0.5, 0.5);
+
+        this.add.sprite(300, 100, 'button1');
+
+        // the last argument is the item on the spritesheet
+        this.chest = this.physics.add.image(300, 300, 'items', 0);
+
+        this.wall = this.physics.add.image(500, 100, 'button1');
+        // set collision to wall
+        this.wall.setImmovable();
+
+        this.player = this.physics.add.image(32, 32, 'characters', 0);
+        // change the size of the object, can specify x, y value, specify 1 value will be for both x and y
+        this.player.setScale(2);
+        // prevent player from going off screen
+        this.player.body.setCollideWorldBounds(true);
+
+        this.physics.add.collider(this.player, this.wall);
+        this.physics.add.overlap(this.player, this.chest, function(player, chest) {
+            goldPickupAudio.play();
+            chest.destroy();
+        });
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+    update() {
+        // for both x and y
+        this.player.setVelocity(0);
+    
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+        }
+    
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-160);
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(160);
+        }
     }
 }
